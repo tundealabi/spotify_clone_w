d2781f35ec6fec4b0fb4163ee599ade10f0318d7
Tunde Alabi <tundealabi4780@gmail.com>
2021-12-18 02:01:33 +0100

    project setup and build layout

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>This is a DOPE spotify app.</h1>
+      <main>
+        {/* Sidebar */}
+        {/* center */}
+      </main>
+      <div>{/* Player */}</div>
+    </div>
+  );
+}
